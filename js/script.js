dd8d32c783ec176e9f0301d2532ca412b655038f
panovoleg <149285809+panovoleg@users.noreply.github.com>
2024-03-02 19:31:55 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//задача 1
+//let items = document.querySelectorAll("li")
+//let json='["city1","city2","city3","city4"]'
+//for (let item of items){
+ //   console.log(item)
+//}
+
+//задание 2
+//let json = '["user1","user2","user3","user4","user5"]';
+//let h=JSON.parse(json);
+//h.push("user6");
+//let g =JSON.stringify(h); 
+//console.log(g);
+
+//задание 3
+//let json = `[
+//	{
+//		"name": "user1",
+//		"age": 25,
+//		"salary": 1000
+//	},
+//	{
+//		"name": "user2",
+//		"age": 26,
+//		"salary": 2000
+//	},
+	//{
+	//	"name": "user3",
+	//	"age": 27,
+	//	"salary": 3000
+	//}
+//]`;
+
+//задание 4
+let items = document.querySelectorAll("li")
+let json='["user1","user2","user3","user4","user5"]'
+for (let item of items){
+    console.log(item)
+}
